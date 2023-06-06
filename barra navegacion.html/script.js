9453077f929e09c0d6823e42b2a4a721168dc1d8
wjg3005 <waltherj37@gmail.com>
2023-06-05 21:16:24 -0500

    mi primer commit

---
+++ barra navegacion.html/script.js
@@ -0,0 +1,6 @@
+/*function showAlert(){ alert("!hola ya estoy programando");}*/
+const miBoton = document.getElementById("miBoton");
+function holaestoyprogramando(){
+    alert ("hola estoy programando");
+} 
+miBoton.addEventListener("click",holaestoyprogramando)
